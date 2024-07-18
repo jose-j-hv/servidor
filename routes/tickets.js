4c7f1758876873d7d816dfe72c14aabce0f3604d
juanvillegas <josejuanhv9512@gmail.com>
2024-07-17 22:04:18 -0600

    first commit

---
+++ routes/tickets.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const ticketController = require('../controllers/ticketController');
+
+router.get('/', ticketController.getAllTickets);
+router.get('/getall', ticketController.getAllTickets);
+router.get('/getone', ticketController.getTicketById);
+router.post('/createTicket', ticketController.createTicket);
+
+
+module.exports = router;
